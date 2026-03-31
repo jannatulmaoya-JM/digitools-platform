986709c7b05437e14df0fc1ca770f605a25972fc
maoya12345 <jannatulmaoya456@gmail.com>
2026-03-31 11:42:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h2 className="text-red-500 bg-black py-8 text-center font-bold text-3xl  ">Hello World</h2>
+   
+    </>
+  )
+}
+
+export default App
